467ef23647ce89715e41f80999b8bfa7f2fa9933
Inseobkim3053 <83901246+Inseobkim3053@users.noreply.github.com>
2021-08-07 17:29:32 +0900

    Add files via upload

---
+++ ProjectImage-Server-main/src/main/java/com/study/projectimageserver/domain/User.java
@@ -0,0 +1,36 @@
+package com.study.projectimageserver.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자를 만들어줍니다.
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
+
+
